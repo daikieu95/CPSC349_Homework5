05a71f72e619b6b05a030fed2908a8048393d4bf
DENNIS KIEU <42823136+daikieu95@users.noreply.github.com>
2019-10-13 15:48:16 -0700

    HW 5

---
+++ chattrbox/app/mime.js
@@ -0,0 +1,27 @@
+function Mime() {
+  this._types = Object.create(null);
+  this._extensions = Object.create(null);
+
+  for (var i = 0; i < arguments.length; i++) {
+    this.define(arguments[i]);
+  }
+
+  this.getType = this.getType.bind(this);
+
+}
+
+/**
+ * Lookup a mime type based on extension
+ */
+Mime.prototype.getType = function(path) {
+  path = String(path);
+  var last = path.replace(/^.*[/\\]/, '').toLowerCase();
+  var ext = last.replace(/^.*\./, '').toLowerCase();
+
+  var hasPath = last.length < path.length;
+  var hasDot = ext.length < last.length - 1;
+
+  return (hasDot || !hasPath) && this._types[ext] || null;
+};
+
+module.exports = Mime;
